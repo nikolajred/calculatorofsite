13cdbc2a6120df7f92bcbe9333d9c4861cf6a580
Nikolay <nikolajred@gmail.com>
2020-11-29 14:09:00 +0200

    second commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
 const startButton = document.querySelector('.start-button');
 console.log(startButton);
 
-console.dir(startButton);
+// console.dir(startButton);
 
-startButton.onclick = function(){
-    console.log("Клик по кнопке!!!");
-}
+// startButton.onclick = function(){
+//     console.log("Клик по кнопке!!!");
+// }
+
+startButton.addEventListener('click', function(){
+    console.log('Клик по кнопке!!!');
+});
